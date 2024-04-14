2027ff9d142c3a0622e870ec26d61fca244f7df4
jonatan galan hernandez <jonatangalanh@gmail.com>
2024-04-14 12:34:32 -0600

    idk

--- example/ejemplomqtt/android/app/src/main/kotlin/com/example/ejemplomqtt/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ejemplomqtt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- example/mqtt/mqtt_example/android/app/src/main/kotlin/com/example/mqtt_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mqtt_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
